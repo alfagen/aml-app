29d41c255f5f2d3a80dc4e4f18a7fe00a6ad196c
Danil Pismenny <danil@brandymint.ru>
2018-08-06 16:35:50 +0300

    Версия в шапке и хелпер пагинации

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def paginate objects, options = {}
+    options.reverse_merge!( theme: 'twitter-bootstrap-3' )
+
+    super( objects, options )
+  end
+
+  def app_title
+    "AML #{AppVersion}"
+  end
 end
