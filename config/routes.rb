1265ae45fcf0eb5629194860f6ffed1863541b98
Valenti nAndreev <valentinandreev80@gmail.com>
2018-08-08 14:41:54 +0300

    [#16] Users managment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@
 Rails.application.routes.draw do
   root to: 'client_documents#index'
   resources :document_kinds, only: %i[index new create]
-  resources :clients, except: %i[edit update destroy]
+  resources :clients, except: %i[destroy] do
+    member do
+      put :unblock
+      put :block
+    end
+  end
   resources :client_documents, only: %i[show index new create] do
     member do
       put :accept
