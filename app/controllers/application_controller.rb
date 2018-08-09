45908b810662363c42ad10544710404b5441987b
Valenti nAndreev <valentinandreev80@gmail.com>
2018-08-09 14:40:54 +0300

    [#16] Check blocked user before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :check_user
+
+  def check_user
+    return unless current_user&.blocked?
+    logout
+    redirect_back_or_to(:users)
+  end
 end
