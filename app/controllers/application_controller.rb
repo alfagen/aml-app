1961a0b4d6905dcb4539068fb6a81cea4810e3b9
Danil Pismenny <danil@brandymint.ru>
2018-08-10 11:36:46 +0300

    Удалил logout_blocked_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :require_login
-  before_action :logout_blocked_user
-
-  def logout_blocked_user
-    return unless current_user&.blocked?
-    logout
-    redirect_to new_user_session_path, alert: 'Пользователь заблокирован'
-  end
 
   def not_authenticated
-    render 'not_authenticated'
+    render 'not_authenticated', layout: 'blank'
   end
 end
