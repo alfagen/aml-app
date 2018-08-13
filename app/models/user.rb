3b9ada6c49d7f8c7c990b9ec1384d6a2a53539b1
ValentinAndreev <valentinandreev80@gmail.com>
2018-08-13 20:17:57 +0300

    [#25] Fix validation of password changing

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
 
   scope :ordered, -> { order 'id desc' }
 
-  validates :password, presence: true
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
