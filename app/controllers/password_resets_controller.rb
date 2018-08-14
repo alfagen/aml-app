2a3d91708ce517228601fe313d790cdba36a4f08
Valenti nAndreev <valentinandreev80@gmail.com>
2018-08-14 13:31:12 +0300

    [#25] Email passwordfixes

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,8 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user&.deliver_reset_password_instructions!
-    redirect_to users_path, notice: 'Отправлен email с ссылкой на смену пароля'
+  rescue Exception
+    flash.now.alert = 'Не удалось отправить email'
   end
 
   def edit
@@ -14,7 +15,6 @@ class PasswordResetsController < ApplicationController
 
   def update
     change_password!
-    redirect_to users_path, flash[:notice] = 'Пароль изменен'
   rescue ActiveRecord::RecordInvalid => e
     flash.now.alert = e.message
     render 'passwords/edit', locals: { user: e.record, token: params[:id] }
