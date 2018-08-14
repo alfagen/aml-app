05b2e6c09cad3ce11cd8325a7b70609de78ba436
Valenti nAndreev <valentinandreev80@gmail.com>
2018-08-14 13:41:36 +0300

    [#25] Email passwordfixes

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,7 +17,7 @@ class PasswordResetsController < ApplicationController
     change_password!
   rescue StandardError
     flash.now.alert = 'Не удалось изменить пароль'
-    render 'passwords/edit', locals: { user: user, token: params[:id] }
+    render 'passwords/edit', locals: { user: user_from_token, token: params[:id] }
   end
 
   private
