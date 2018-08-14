d82e204641ce85d821e282ca8e4d7b84cfd23a97
Valenti nAndreev <valentinandreev80@gmail.com>
2018-08-14 15:53:10 +0300

    [#25] Email passwordfixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,9 @@ class User < ApplicationRecord
 
   scope :ordered, -> { order 'id desc' }
 
-  validates :password, presence: true
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 10 }, if: -> { :crypted_password_changed? }
+  validates :password, confirmation: true, if: -> { new_record? || :crypted_password_changed? }
+  validates :password_confirmation, presence: true, if: -> { new_record? || :crypted_password_changed? }
   validates :email, presence: true, uniqueness: true, email: true
 
   after_commit :deliver_reset_password_instructions!, on: :create
