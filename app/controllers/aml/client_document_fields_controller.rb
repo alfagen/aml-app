46fd19ec782b7e145079575df632846c40086eca
ValentinAndreev <valentinandreev80@gmail.com>
2018-08-28 09:17:08 +0300

    [#62] Namespace

--- app/controllers/aml/client_document_fields_controller.rb
+++ app/controllers/aml/client_document_fields_controller.rb
@@ -9,11 +9,9 @@ module Aml
     def update
       client_document_field.update!(permitted_params)
       redirect_to client_document_path(client_document)
-    rescue ActiveRecord::RecordInvalid => e
-      flash.now.alert = e.message
-      render :edit, locals: { client_document_field: e.record,
-                              client_document: e.record.client_document,
-                              document_kind_field_definition: e.record.definition }
+    rescue ActiveRecord::RecordInvalid => error
+      flash.now.alert = error.message
+      render :edit, locals: error_params(error)
     end
 
     private
@@ -28,6 +26,12 @@ module Aml
 
     def permitted_params
       params.fetch(:aml_client_document_field, {}).permit(:value)
+    end
+
+    def error_params(error)
+      { client_document_field: error.record,
+        client_document: error.record.client_document,
+        document_kind_field_definition: error.record.definition }
     end
   end
 end
