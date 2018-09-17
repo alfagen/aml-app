5aa7ac5251acf0e63149409f61884e66e7f73fd0
ValentinAndreev <valentinandreev80@gmail.com>
2018-09-17 10:35:29 +0300

    update spec

--- spec/controllers/aml/order_document_controller_spec.rb
+++ spec/controllers/aml/order_document_controller_spec.rb
@@ -4,11 +4,19 @@ RSpec.describe AML::OrderDocumentsController, type: :controller do
   describe '#actions' do
     let(:aml_operator) { create(:operator) }
     let(:aml_document_kind) { create(:document_kind) }
-    let!(:aml_client) { create(:client) }
+    let(:aml_client) { create(:client) }
     let(:aml_order) { create(:order, client_id: aml_client.id) }
     let(:aml_order_document) { create(:order_document, order_id: aml_order.id) }
 
     before { login_user(aml_operator) }
+
+    context '#update' do
+      it 'update' do
+        put 'update', params: { order_document: { order_id: aml_order.id,
+                                                  document_kind: aml_document_kind.id,
+                                                  client_id: aml_client.id }, id: aml_order_document.id }
+      end
+    end
 
     context '#show' do
       subject { get :show, params: { id: aml_order_document.id } }
--- spec/factories/order_documents.rb
+++ spec/factories/order_documents.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory(:order_document, class: AML::OrderDocument) do
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'test_files', 'test.png')) }
-    workflow_state 'pending'
     order
     document_kind
   end
