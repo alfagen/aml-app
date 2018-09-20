df370fb5f6baffa979d55f4d5cba1719de2067d1
ValentinAndreev <valentinandreev80@gmail.com>
2018-09-20 09:49:11 +0300

    document-group-kinds

--- app/controllers/amlapp/document_kinds_controller.rb
+++ app/controllers/amlapp/document_kinds_controller.rb
@@ -5,34 +5,33 @@ module Amlapp
     include Pagination
 
     def index
-      render :index, locals: { document_kinds: paginate(AML::DocumentKind.ordered) }
+      render :index, locals: { document_kinds: paginate(document_group.document_kinds.ordered), document_group: document_group }
     end
 
     def new
-      render :new, locals: { document_kind: AML::DocumentKind.new(permitted_params), document_groups: document_groups }
+      render :new, locals: { document_kind: AML::DocumentKind.new(permitted_params), document_group: document_group }
     end
 
     def create
       AML::DocumentKind.create!(permitted_params)
-      redirect_to document_kinds_path
+      render :index, locals: { document_kinds: paginate(document_group.document_kinds.ordered), document_group: document_group }
     rescue ActiveRecord::RecordInvalid => e
       flash.now.alert = e.message
-      render :new, locals: { document_kind: e.record, document_groups: document_groups }
+      render :new, locals: { document_kind: e.record, document_group: document_group }
     end
 
     def show
-      render :show, locals: { document_kind: document_kind,
-                              field_definitions: document_kind.definitions }
+      render :show, locals: { document_kind: document_kind }
     end
 
     private
 
-    def document_groups
-      @document_groups ||= AML::DocumentGroup.all
-    end
-
     def document_kind
       @document_kind ||= AML::DocumentKind.find params[:id]
+    end
+
+    def document_group
+      @document_group ||= AML::DocumentGroup.find permitted_params[:aml_document_group_id]
     end
 
     def permitted_params
--- spec/controllers/document_kinds_controller_spec.rb
+++ spec/controllers/document_kinds_controller_spec.rb
@@ -3,18 +3,19 @@ require 'rails_helper'
 RSpec.describe Amlapp::DocumentKindsController, type: :controller do
   describe '#base actions' do
     let(:aml_operator) { create(:operator) }
+    let(:aml_document_group) { create(:document_group) }
     let(:aml_document_kind) { create(:document_kind) }
 
     before { login_user(aml_operator) }
 
     context 'with registered operator' do
       it '#create' do
-        post :create, params: { aml_document_kind: attributes_for(:document_kind) }
+        post :create, params: { document_kind: attributes_for(:document_kind, aml_document_group_id: aml_document_group.id) }
         expect(response).to be_successful
       end
 
       it '#index' do
-        get :index
+        get :index, params: { document_kind: attributes_for(:document_kind, aml_document_group_id: aml_document_group.id) }
         expect(response).to be_successful
       end
 
