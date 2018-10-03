f84947cab060a7c7bbcbe8e0d5cba277dc0a7430
Danil Pismenny <danil@brandymint.ru>
2018-10-03 16:37:36 +0300

    minor

--- app/authorizers/aml/status_authorizer.rb
+++ app/authorizers/aml/status_authorizer.rb
@@ -1,15 +1,4 @@
 module AML
   class StatusAuthorizer < ApplicationAuthorizer
-    def self.creatable_by?(operator)
-      operator.administrator?
-    end
-
-    def processable_by?(operator)
-      operator.administrator?
-    end
-
-    def readable_by?(operator)
-      operator.administrator?
-    end
   end
 end
