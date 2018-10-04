f2725825550c56f4bd81d9e5a6e6755a3d4c68f0
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-04 16:29:46 +0300

    fix syntax

--- app/authorizers/aml/order_document_authorizer.rb
+++ app/authorizers/aml/order_document_authorizer.rb
@@ -2,16 +2,19 @@ module AML
   class OrderDocumentAuthorizer < ApplicationAuthorizer
     EVENTS = %i[accept reject].freeze
     OWNER_REQUIRED_FOR_EVENTS = %i[accept reject].freeze
-     def self.readable_by?(_user)
+
+    def self.readable_by?(_user)
       true
     end
-     EVENTS.each do |event|
+
+    EVENTS.each do |event|
       ability = Authority.abilities[event] || raise("No ability for event #{event}")
       define_singleton_method "#{ability}_by?" do |_operator|
         true
       end
     end
-     EVENTS.each do |event|
+
+    EVENTS.each do |event|
       ability = Authority.abilities[event] || raise("No ability for event #{event}")
       define_method "#{ability}_by?" do |operator|
         resource.enabled_workflow_events.include?(event) \
