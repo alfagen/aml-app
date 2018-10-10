75e5c330ad4be699e191b9efd0adb34aca6b83c3
Danil Pismenny <danil@brandymint.ru>
2018-10-10 15:54:28 +0300

    Ловим и отправляем в bugsnag исключения в ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,15 @@
 
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
+  # default from: Settings.mailer.default_from
+  #
   layout 'mailer'
+
+  # советуют ловить только ActiveJob::DeserializationError
+  # но мне интересно что там вообще может падать
+  rescue_from StandardError do |exception|
+    Rails.logger.error exception
+    Bugsnag.notify exception
+    raise exception
+  end
 end
