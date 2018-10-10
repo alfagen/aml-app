3356a056856c9d0441dad49cc0334b01aafc5a47
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-10 11:47:42 +0300

    document-reason-reject

---
+++ db/migrate/20181010072805_add_reference_to_order_documents_to_reject_reason.aml.rb
@@ -0,0 +1,8 @@
+# This migration comes from aml (originally 20181010070944)
+class AddReferenceToOrderDocumentsToRejectReason < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :aml_order_documents, :aml_reject_reason
+    add_column :aml_reject_reasons, :kind, :integer
+    add_column :aml_order_documents, :reject_reason_details, :text
+  end
+end
