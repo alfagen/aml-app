762dd7b76c99b5a9a4b5e5c34d5bca4d8ca4d104
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-11 13:45:55 +0300

    move validation to model

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,8 +4,7 @@ class PasswordsController < ApplicationController
   end
 
   def update
-    check_password
-    current_user.update!(permitted_params.except(:current_password))
+    current_user.update!(permitted_params)
     if permitted_params[:password].present?
       flash.now.notice = 'Пароль изменен'
     else
@@ -14,19 +13,12 @@ class PasswordsController < ApplicationController
     redirect_to orders_path
   rescue ActiveRecord::RecordInvalid => e
     flash.now.alert = e.message
-    render :edit, locals: { user: current_user }
+    render :edit, locals: { user: e.record }
   end
 
   private
 
   def permitted_params
     params.require(:operator).permit(:email, :current_password, :password, :password_confirmation)
-  end
-
-  def check_password
-    unless current_user.valid_password? permitted_params[:current_password]
-      current_user.errors.add(:current_password, 'Текущий пароль не верен.')
-      raise ActiveRecord::RecordInvalid.new(current_user)
-    end
   end
 end
