d75293db2b4e06c00afb5d5f2eb24890c3be8f2e
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-11 11:53:28 +0300

    fix specs

--- spec/controllers/amlapp/passwords_controller_spec.rb
+++ spec/controllers/amlapp/passwords_controller_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe PasswordsController, type: :controller do
   describe '#update' do
     context 'with registered user' do
-      let(:aml_operator) { create(:aml_operator) }
+      let(:aml_operator) { create(:aml_operator, password: 'password') }
       let(:password) { 'new_random_password' }
 
       before { login_user(aml_operator) }
 
       before do
-        put 'update', params: { operator: attributes_for(:aml_operator, password: password, password_confirmation: password) }
+        put 'update', params: { operator: attributes_for(:aml_operator, current_password: 'password', password: password, password_confirmation: password) }
       end
 
       it 'chahge password' do
