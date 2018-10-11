efe981aab5d87de65c8c7717d652f7626379acfb
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-11 10:08:23 +0300

    Delete 20181011062250_add_goal_to_document_kind.aml.rb

--- db/migrate/20181011062250_add_goal_to_document_kind.aml.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from aml (originally 20181010124654)
-class AddGoalToDocumentKind < ActiveRecord::Migration[5.2]
-  def change
-    add_column :aml_document_kinds, :goal, :string
-  end
-end
