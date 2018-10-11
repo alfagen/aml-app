a86713c240dcaf821bed03338254341e981ce5db
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-11 11:47:33 +0300

    check current password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,7 +4,8 @@ class PasswordsController < ApplicationController
   end
 
   def update
-    current_user.update!(permitted_params)
+    check_password
+    current_user.update!(permitted_params.except(:current_password))
     if permitted_params[:password].present?
       flash.now.notice = 'Пароль изменен'
     else
@@ -13,12 +14,19 @@ class PasswordsController < ApplicationController
     redirect_to orders_path
   rescue ActiveRecord::RecordInvalid => e
     flash.now.alert = e.message
-    render :edit, locals: { user: e.record }
+    render :edit, locals: { user: current_user }
   end
 
   private
 
   def permitted_params
-    params.require(:operator).permit(:email, :password, :password_confirmation)
+    params.require(:operator).permit(:email, :current_password, :password, :password_confirmation)
+  end
+
+  def check_password
+    unless current_user.valid_password? permitted_params[:current_password]
+      current_user.errors.add(:current_password, 'Текущий пароль не верен.')
+      raise ActiveRecord::RecordInvalid.new(current_user)
+    end
   end
 end
