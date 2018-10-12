60825d212c4b302bc33c23a6d43bb3f0afd2666a
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-12 11:07:47 +0300

    rubocop in travis

--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -23,7 +23,9 @@ RSpec.describe PasswordsController, type: :controller do
         # сначала создаем пароль
         put 'update', params: { operator: attributes_for(:aml_operator, password: current_password, password_confirmation: current_password) }
         # потом его меняем
-        put 'update', params: { operator: attributes_for(:aml_operator, current_password: current_password, password: new_password, password_confirmation: new_password) }
+        put 'update', params: { operator: attributes_for(:aml_operator, current_password: current_password,
+                                                                        password: new_password,
+                                                                        password_confirmation: new_password) }
         expect(AML::Operator.authenticate(aml_operator.email, new_password)).to eq(aml_operator)
         expect(response).to be_redirect
       end
