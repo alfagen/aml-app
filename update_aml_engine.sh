0bb4ce566052c911708db4a0e9959eab9bd83678
Danil Pismenny <danil@brandymint.ru>
2018-10-12 14:05:32 +0300

    Добавит update_aml_engine.sh

---
+++ update_aml_engine.sh
@@ -0,0 +1,28 @@
+#/usr/bin/env sh
+
+CURRENT_VERSION=`ruby -r./vendor/aml-engine/lib/aml/version -e "print AML::VERSION"`
+
+cd ./vendor/aml-engine
+git checkout master && git pull > /dev/null
+# make all
+cd ../../
+pwd
+
+NEW_VERSION=`ruby -r./vendor/aml-engine/lib/aml/version -e "print AML::VERSION"`
+
+if [ "${CURRENT_VERSION}" = "${NEW_VERSION}" ]; then
+  echo "Обновление прекращено: Версия не изменилась (${CURRENT_VERSION})"
+  exit 1
+fi
+
+MESSAGE="update aml-engine from ${CURRENT_VERSION} to ${NEW_VERSION}"
+
+rails aml:install:migrations
+
+echo "Создаю и заливаю комит '${MESSAGE}'"
+git add ./db/
+git add ./vendor/aml-engine
+git commit -m "${MESSAGE}"
+git push
+
+echo 'Можно релизить'
