0e670b15dc41703345d9c02850e72ca61b80276d
ValentinAndreev <valentinandreev80@gmail.com>
2018-10-15 19:38:21 +0300

    form-object start

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,26 +1,16 @@
 class PasswordsController < ApplicationController
   def edit
-    render :edit, locals: { change_password_form: change_password_form }
+    render :edit, locals: { change_password_form: ChangePasswordForm.new }
   end
 
   def update
-    current_user.change_password! form_params[:password]
-
-    flash.now.notice = 'Пароль изменен'
-
+    current_user.update! permitted_params
     redirect_to orders_path
-  rescue ActiveRecord::RecordInvalid => e
-    flash.now.alert = e.message
-    render :edit, locals: { change_password_form: change_password_form }
   end
 
   private
 
-  def form_params
-    params.fetch(:change_password_form, {}).permit(:current_password, :password, :password_confirmation)
-  end
-
-  def change_password_form
-    @change_password_form ||= ChangePasswordForm.new(form_params.merge(user_id: current_user.id))
+  def permitted_params
+    params.require(:change_password_form).permit(:password, :password_confirmation)
   end
 end
--- app/models/change_password_form.rb
+++ app/models/change_password_form.rb
@@ -4,24 +4,7 @@ class ChangePasswordForm
   extend  ActiveModel::Naming
   include ActiveModel::Validations
 
-  attribute :user_id, Integer
-  attr_accessor :current_password
-
-  validate :validate_current_password, if: :current_password_required?
-  validates :current_password, presence: true, if: :current_password_required?
-
-  private
-
-  def validate_current_password
-    return if user.valid_password? current_password
-    errors.add :current_password, 'Текущий пароль не верен.'
-  end
-
-  def current_password_required?
-    user.crypted_password? && user.crypted_password_was.present? && user.persisted?
-  end
-
-  def user
-    @user ||= AML::Operator.find_by id: user_id
-  end
+  attribute :current_password, String
+  attribute :password, String
+  attribute :password_confirmation, String
 end
