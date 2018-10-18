89c7ec424aba5fd0895e13ea6d8cf47c1e0508c5
Danil Pismenny <danil@brandymint.ru>
2018-10-18 15:50:21 +0300

    fix mailer from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: Settings.mailer.default_from
+  default from: Settings.mailer.from
 
   layout 'mailer'
 
