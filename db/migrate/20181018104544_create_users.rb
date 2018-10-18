84de1387aa43c3e47ad1c95418b3d2d795f11b32
ValentinAndreev <valentinandreev80@gmail.com>
2018-10-18 14:20:17 +0300

    fix migration

--- db/migrate/20181018104544_create_users.rb
+++ db/migrate/20181018104544_create_users.rb
@@ -1,5 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :users do |t|
       t.string :email, null: false
       t.string :crypted_password
@@ -12,7 +12,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     add_index :users, :email, unique: true
 
     AML::Operator.all.each do |operator|
-      User.create!(email: operator.email, crypted_password: operator.crypted_password, salt: operator.salt)
+      User.create!(email: operator.email, crypted_password: operator.crypted_password, salt: operator.salt, aml_operator_id: operator.id)
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
