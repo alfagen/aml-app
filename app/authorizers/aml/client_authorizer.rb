5b9b55fb9b5158f47e998703f148eda69852d9c4
Danil Pismenny <danil@brandymint.ru>
2018-10-19 17:23:51 +0300

    ресет клиенту можно делать всегда

--- app/authorizers/aml/client_authorizer.rb
+++ app/authorizers/aml/client_authorizer.rb
@@ -1,8 +1,6 @@
 module AML
   class ClientAuthorizer < ApplicationAuthorizer
     def resetable_by?(operator)
-      return false if resource.aml_status == AML.default_status || resource.aml_status.nil?
-
       updatable_by? operator
     end
   end
