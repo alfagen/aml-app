8b716e186ffe5a9d634b7e7bb68e92aa3f736069
Danil Pismenny <danil@brandymint.ru>
2018-10-19 14:15:44 +0300

    staging для database

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = Settings.default_url_options.symbolize_keys
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = Secrets.mailtrap.smtp_settings.symbolize_keys
+  # config.action_mailer.smtp_settings = Secrets.mailtrap.smtp_settings.symbolize_keys
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
