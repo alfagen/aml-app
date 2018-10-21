20ab87fda6262704aef07eacd3c8a866dc806bca
Danil Pismenny <danil@brandymint.ru>
2018-10-21 14:26:51 +0300

    fix локаль в application_controller

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,11 +1,8 @@
 class LocalesController < ApplicationController
-  include SetLocale
-
   def update
     locale = available_locale params[:locale]
     if locale.present?
       I18n.locale = session[:locale] = locale
-      flash.notice = "Вы установили локаль: #{locale}"
     else
       flash.alert = "Такая локаль #{params[:locale]} не поддерживается"
     end
