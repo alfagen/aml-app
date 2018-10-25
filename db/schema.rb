e6cf2286b067bbd8222ffcb8740eb7f54bf93199
ValentinAndreev <valentinandreev80@gmail.com>
2018-10-25 15:52:45 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_104544) do
+ActiveRecord::Schema.define(version: 2018_10_23_101211) do
 
   create_table "aml_clients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
