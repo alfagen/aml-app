1b060708aef19fb9e3ac4bf05e00e96e1a738121
ValentinAndreev <valentinandreev80@gmail.com>
2018-10-30 12:20:03 +0300

    travis

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     crypted_password { 'MyString' }
     salt { 'MyString' }
+    sequence(:name) { |n| "user#{n}" }
 
     trait :operator do
       association :aml_operator, factory: :aml_operator
