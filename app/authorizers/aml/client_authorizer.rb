40be4948a932fb866461d8423fb2b34a70eaad51
Danil Pismenny <danil@brandymint.ru>
2018-11-07 13:19:24 +0300

    Доступ к клиентским карточкам для оператора

--- app/authorizers/aml/client_authorizer.rb
+++ app/authorizers/aml/client_authorizer.rb
@@ -8,6 +8,14 @@ module AML
       true
     end
 
+    def self.readable_by?(_operator)
+      true
+    end
+
+    def readable_by?(_operator)
+      true
+    end
+
     def resetable_by?(operator)
       updatable_by? operator
     end
