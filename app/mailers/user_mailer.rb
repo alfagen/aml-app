4b085d44b085fdb661d113986aced6f553768f4b
Danil Pismenny <danil@brandymint.ru>
2018-11-19 19:11:28 +0300

    fix specs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < AML::ApplicationMailer
   layout 'mailer'
 
+  default from: Settings.mailer.from
+
   def reset_password_email(user)
     @url = edit_password_reset_url(user.reset_password_token)
     mail(to: user.email, subject: 'Вы зарегистрированы')
