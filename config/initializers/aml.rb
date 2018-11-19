2aa4c1989e3e4713801846867dc69ac8baba216d
Danil Pismenny <danil@brandymint.ru>
2018-11-19 10:21:14 +0300

    Вынес настройки AML в AML.configure

--- config/initializers/aml.rb
+++ config/initializers/aml.rb
@@ -1,11 +1,15 @@
-# Rails.application.config.after_initialize do
-Rails.application.config.to_prepare do
+AML.configure do |config|
+  config.allowed_emails = Secrets.aml_allowed_emails
+  config.mail_from = Settings.mailer.from
+  config.logger = ActiveSupport::Logger.new Rails.root.join './log/aml.log'
+end
+
+Rails.application.config.after_initialize do
   [
-    AML::Order, AML::Client, AML::Operator, AML::OrderDocument, AML::Status, AML::RejectReason,
-    AML::DocumentKindFieldDefinition, AML::DocumentKind, AML::DocumentGroup, AML::DocumentField,
-    AML::CheckList, AML::OrderCheck, AML::Agreement, AML::Notification, AML::NotificationTemplate,
-    AML::ClientInfo
+    AML::Order, AML::Client, AML::Operator, AML::OrderDocument,
+    AML::DocumentKindFieldDefinition, AML::DocumentKind, AML::DocumentGroup, AML::DocumentField
   ].each { |model| model.include Authority::Abilities }
 
   AML::Operator.include Authority::UserAbilities
+  AML::Operator.authenticates_with_sorcery!
 end
