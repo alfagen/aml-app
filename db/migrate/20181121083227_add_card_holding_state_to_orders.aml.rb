65e4c882b046e6d4ae3e18f6eb82b19dcc4b5381
Danil Pismenny <danil@brandymint.ru>
2018-11-21 11:35:29 +0300

    Статус холдирования карты

---
+++ db/migrate/20181121083227_add_card_holding_state_to_orders.aml.rb
@@ -0,0 +1,10 @@
+# This migration comes from aml (originally 20181121081848)
+class AddCardHoldingStateToOrders < ActiveRecord::Migration[5.2]
+  def change
+    add_column :aml_orders, :card_holding_state, :string, default: 'none', null: false
+    remove_column :aml_orders, :card_holded
+
+    remove_column :aml_orders, :card_holded_at
+    add_column :aml_orders, :card_holding_state_updated_at, :timestamp
+  end
+end
