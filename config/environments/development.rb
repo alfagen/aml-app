978c637b5dd81eddff855e408e7ed315820101be
Valentin Andreev <valentinandreev80@gmail.com>
2018-11-21 13:12:20 +0300

    проверка key в development если есть (#166)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.require_master_key = true
+  config.require_master_key = true if ENV['RAILS_MASTER_KEY'].present?
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
