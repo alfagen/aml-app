6cfaca9b1c3298a9a65daa506d6d1845a90c77a7
Danil Pismenny <danil@brandymint.ru>
2018-11-22 13:02:04 +0300

    deploy to staging0

--- config/deploy/staging0.rb
+++ config/deploy/staging0.rb
@@ -9,4 +9,4 @@ set :bundle_without, %w[test].join(' ')
 set :keep_releases, 2
 
 # http://aml.stage.bang8.ru
-server 'aml.stage.bang8.ru', port: '8122', user: fetch(:user), roles: %w[web app db bugsnag].freeze
+server 'aml.stage.bang8.ru', user: fetch(:user), roles: %w[web app db bugsnag].freeze
