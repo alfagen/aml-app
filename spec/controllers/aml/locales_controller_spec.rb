409a6178eb5539e4e5cdc2474c6f8463d5ac7f54
ValentinAndreev <valentinandreev80@gmail.com>
2018-11-26 10:47:45 +0300

    нет необходимости to_sym

--- spec/controllers/aml/locales_controller_spec.rb
+++ spec/controllers/aml/locales_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe AML::LocalesController, type: :controller do
 
       it 'изменение локали на другую' do
         locale = I18n.locale
-        another_locale = (I18n.available_locales - [locale.to_sym]).sample || :en
+        another_locale = (I18n.available_locales - [locale]).sample || :en
         put 'update', params: { locale: another_locale }
         expect(I18n.locale).to eq(another_locale)
       end
