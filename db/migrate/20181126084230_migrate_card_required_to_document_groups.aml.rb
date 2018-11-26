215dad1f1a2f17a97f86c26ac4dbcc10e42a18c3
Danil Pismenny <danil@brandymint.ru>
2018-11-26 11:42:58 +0300

    update aml engine

---
+++ db/migrate/20181126084230_migrate_card_required_to_document_groups.aml.rb
@@ -0,0 +1,8 @@
+# This migration comes from aml (originally 20181123133517)
+class MigrateCardRequiredToDocumentGroups < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :aml_orders, :card_image
+    remove_column :aml_statuses, :card_required
+    add_column :aml_document_groups, :card_required, :boolean, null: false, default: false
+  end
+end
