342d9e88c26eaffbd8b122fea8ba9677baf4ca8a
Valentin Andreev <valentinandreev80@gmail.com>
2018-12-19 20:24:17 +0300

    конфиги для card

--- config/initializers/aml.rb
+++ config/initializers/aml.rb
@@ -1,6 +1,9 @@
 AML.configure do |config|
   config.allowed_emails = Secrets.aml_allowed_emails
   config.mail_from = Settings.mailer.from
+  config.card_brands = Settings.card.brands
+  config.card_bin = Settings.card.bin
+  config.card_suffix = Settings.card.suffix
   config.logger = ActiveSupport::Logger.new Rails.root.join './log/aml.log'
   config.logger.formatter = AutoLogger::Formatter.new
 end
