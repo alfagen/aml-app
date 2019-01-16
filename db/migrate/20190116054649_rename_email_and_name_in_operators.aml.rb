f905a22b6ef62b616bcd93ee833cfb176a3f4a7c
ValentinAndreev <valentinandreev80@gmail.com>
2019-01-16 08:48:29 +0300

    миграции переименования email оператора

---
+++ db/migrate/20190116054649_rename_email_and_name_in_operators.aml.rb
@@ -0,0 +1,10 @@
+# This migration comes from aml (originally 20190114121523)
+class RenameEmailAndNameInOperators < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :aml_operators, :email, :legacy_email
+    rename_column :aml_operators, :name, :legacy_name
+
+    change_column_null :aml_operators, :legacy_email, true
+    change_column_null :aml_operators, :legacy_name, true
+  end
+end
